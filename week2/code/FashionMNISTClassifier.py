b8fd52212dd3e7899a7a571457dcca3a8818e6cc
Andrzej <andreinkfemust@gmail.com>
2020-11-29 10:58:57 +0100

    update

--- week2/code/FashionMNISTClassifier.py
+++ week2/code/FashionMNISTClassifier.py
@@ -1,25 +1,41 @@
-import torch
+import torch as nn
 import matplotlib.pyplot as plt
 from dataloader import Dataset
 from load_mnist import load_mnist
 
+def get_block(input_dim, output_dim):
+    return nn.Sequential(
+        nn.Linear(input_dim, output_dim),
+        nn.BatchNorm1d(output_dim),
+        nn.ReLU(inplace=True),
+    )
 
-class Network(torch.nn.Module):
-    def __init__(self, in_feat, out_feat):
-        super(Network, self).__init__()
-        self.linear = torch.nn.Linear(in_feat, out_feat)
+class FashionMNISTClassifier(nn.Module):
+    def __init__(self, in_feat, out_feat, num_neurons=(50,10)):
+        super(FashionMNISTClassifier, self).__init__()
+        #self.linear = nn.Linear(in_feat, out_feat)
+        self.linear = nn.Sequential(
+            get_block(in_feat, num_neurons[0]),
+            get_block(num_neurons[0], num_neurons[1]),
+            get_block(num_neurons[1], out_feat),
+            nn.Softmax()
+        )
 
     def forward(self, x):
-        return torch.sigmoid(self.linear(x))
+        return nn.sigmoid(self.linear(x))
 
 
 def train(model, dataloader, optimizer):
     epoch_loss = 0
     for iter, (img, label) in enumerate(dataloader):
         predict = model(img)
-        loss = torch.nn.functional.mse_loss(predict,
+        # loss = nn.functional.mse_loss(predict,
+        #                                     label.reshape(-1, 1),
+        #                                     reduction='mean')
+        loss = nn.functional.BCEWithLogitsLoss(predict,
                                             label.reshape(-1, 1),
                                             reduction='mean')
+
         epoch_loss += loss.item()
         if iter % 100 == 0:
             print("Iteration {} loss is: {:.4f}".format(iter, loss.item()))
@@ -35,14 +51,14 @@ if __name__ == '__main__':
 
     dataset_path = "dataset"
     train_dataset = Dataset(dataset_path, 'training')
-    train_dataloader = torch.utils.data.DataLoader(train_dataset,
+    train_dataloader = nn.utils.data.DataLoader(train_dataset,
                                                    batch_size=48,
                                                    shuffle=True,
                                                    drop_last=False)
 
     # output dim is 1 for label prediction
     model = Network(28 * 28, 1)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.005)
+    optimizer = nn.optim.SGD(model.parameters(), lr=0.005)
 
     # training for certain epochs
     epochs = 25
@@ -61,7 +77,7 @@ if __name__ == '__main__':
 
     # report accuracy on test dataset
     test_dataset = Dataset(dataset_path, 'training')
-    test_dataloader = torch.utils.data.DataLoader(
+    test_dataloader = nn.utils.data.DataLoader(
         test_dataset,
         batch_size=test_dataset.__len__(),
         shuffle=True,
@@ -69,8 +85,8 @@ if __name__ == '__main__':
     for test_imgs, test_label in test_dataloader:
         test_predict = model.forward(test_imgs)
         # test_predict label to 0 or 1
-        test_predict[torch.where(test_predict < 0.5)] = 0.0
-        test_predict[torch.where(test_predict >= 0.5)] = 1.0
+        test_predict[nn.where(test_predict < 0.5)] = 0.0
+        test_predict[nn.where(test_predict >= 0.5)] = 1.0
         accuracy = (test_predict == test_label.reshape(
             -1, 1)).sum().data.numpy().item() / test_dataset.__len__()
 
